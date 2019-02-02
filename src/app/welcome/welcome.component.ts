2cb86e2067df90ab071c78aafa74c3a61a9133a7
NP <pedjastojnic@gmail.com>
2019-02-02 18:36:47 +0100

    Initial commit

---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { AppComponent } from '../app.component';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent implements OnInit {
+
+  constructor(private router: Router, private AppComponent :  AppComponent) { }
+
+
+  ngOnInit() {
+    if(this.AppComponent.loggedIn){
+      this.router.navigate(['/all-rides']);
+    }
+  }
+
+  login(){
+    this.router.navigate(['/login']);
+  }
+
+  signup(){
+    this.router.navigate(['/signup']);
+  }
+
+
+
+}
